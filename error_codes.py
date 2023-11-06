3997aa7393b131edd0341c4b7c2d8d78a64bc682
Ivan <103575282+DragonIvanRussia@users.noreply.github.com>
2023-11-07 00:11:44 +0300

    update

--- error_codes.py
+++ error_codes.py
@@ -3,5 +3,6 @@ error_codes = {
     "NoSettingsFile":"!! Cannot locate the file. Creating a new one...",
     "NoCustomStylesheet":"!! Cannot open the stylesheet. Importing the built-in one...",
     "NoStylesheet":"!! Cannot open the default stylesheet. Continuing without one...",
-    "NoDBFile":"!! Cannot find the layer database. Creating a new one..."
+    "NoDBFile":"!! There was an error with processing the database file...",
+    "GenericError": "!! There was an error in the program. The error code:"
 }
